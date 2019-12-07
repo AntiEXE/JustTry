b9fa0816998e6800b021b30e9e306c08f0e79378
m00nw41k <prince291198@gmail.com>
2019-12-08 02:54:14 +0530

    added comments

---
+++ app/src/main/java/com/example/android/justtry/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.justtry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+// lets seeess  seec
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
